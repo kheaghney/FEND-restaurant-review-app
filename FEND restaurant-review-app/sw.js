b813c1c463b78ebe76dd1198cc062a12bd69e779
Kevin Heaghney <kheaghney1@gmail.com>
2019-03-30 10:49:08 -0500

    Add files via upload

--- FEND restaurant-review-app/sw.js
+++ FEND restaurant-review-app/sw.js
@@ -1,8 +1,26 @@
 let staticCacheName = 'restaurant-static-v1';
-
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/',
+  '/styles/main.css',
+  '/script/main.js'
+];
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+  
 self.addEventListener('install', function (event) {
 	event.waitUntil(
 		caches.open(staticCacheName).then(function (cache) {
+			console.log('Opened cache');
 			return cache.addAll([
 				'./',
 				'./index.html',
@@ -30,8 +48,44 @@ self.addEventListener('install', function (event) {
 		);
 	/* body... */
 });
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+				return fetch(event.request).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
+});
+
+
 self.addEventListener('activate',function (event) {
 	/* body... */
+	var cacheWhitelist = ['pages-cache-v1', 'blog-posts-cache-v1'];
 	event.waitUntil(
 		caches.keys()
 		.then(function (cacheNames) {
@@ -43,35 +97,11 @@ self.addEventListener('activate',function (event) {
 						cacheName != staticCacheName;
 				}).map(function (cacheName) {
 					/* body... */
+					if (cacheWhitelist.indexOf(cacheName) === -1) {
 					return caches.delete(cacheName);
-				})
-				);
-		})
+				}
+			})
 		);
-})
-self.addEventListener('fetch',function (event) {
-	event.respondWith(
-		caches.match(event.request)
-		.then(function (response) {
-			/* body... */
-			return response || fetch(event.request); 
-		})
-		);
-	/* body... */
+	})
+);
 });
-
-
-if('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./service_worker.js', {scope: './'})
-    .then (function(registration) {
-      //Registration worked
-      console.log('Service worker registration succeeded:', registration);
-    })
-    .catch (function(error) {
-          //Registration failed
-          console.log('Registration failed with ' + error);
-    });
-  }
-  
-
-
